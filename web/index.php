f62b7364b567f86668625f729ebaa38b15d3f007
AnonymousX86 <jakub.suchenek.25@gmail.com>
2020-10-29 09:47:30 +0100

    Create base web files

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <?php require_once "static/head.html" ?>
+    <title>Ośrodek wypoczynkowy</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Ośrodek wypoczynkowy</h1>
+            <p>Projekt klasy 4J z Zespołu Szkół Ekonomicznych im. Mikołaja Kopernika w Olsztynie.</p>
+        </div>
+    </div>
+</div>
+<?php require_once "static/scripts.html" ?>
+</body>
+</html>
