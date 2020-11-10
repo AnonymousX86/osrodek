42bb89ab9cbfd610d8316b2b78fa1818c66fd1a4
AnonymousX86 <jakub.suchenek.25@gmail.com>
2020-11-10 09:55:41 +0100

    Poprawiona estetyka

--- web/scripts/update_info.php
+++ web/scripts/update_info.php
@@ -5,14 +5,13 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-if(isset($_POST['text']))
-{
+if(isset($_POST['text'])) {
     require "../env/connect.php";
     $sql = $mysqli->prepare('UPDATE articles SET content = ?, author_id = ?, edit_date = CURRENT_DATE WHERE id=1');
-            $sql->bind_param('si', $_POST['text'], $_SESSION['user_id']);
-            $sql->execute();
-            $mysqli->close();
     header("Location: ../panel/");
+    $sql->bind_param('si', $_POST['text'], $_SESSION['user_id']);
+    $sql->execute();
+    $mysqli->close();
 }
 else
 {
