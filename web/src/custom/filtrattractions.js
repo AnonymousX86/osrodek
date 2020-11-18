bec6d777a11dc450c3cc74dc46145c117e5bcd90
AnonymousX86 <jakub.suchenek.25@gmail.com>
2020-11-18 13:32:57 +0100

    Zaktualizowano nazwy zmiennych i format kodu

--- web/src/custom/filtrattractions.js
+++ web/src/custom/filtrattractions.js
@@ -1,20 +1,11 @@
-function maxkm()
-{
-
-var wiersze=document.getElementsByClassName("obiekt");    
-var poletxt=Number(document.getElementById("maxd").value);   
-
-for(let i=0; i<wiersze.length; i++)    
-{
-let wiersz=wiersze[i].dataset.distance;
-console.log(wiersz);
-
-if(wiersz>poletxt)    
-{
-wiersze[i].style.display="none";
-}
-    
-}
-    
-    
+function maxKm() {
+    const wiersze = document.getElementsByClassName("obiekt");
+    const poleTxt = Number(document.getElementById("maxD").value);
+    for (let i = 0; i < wiersze.length; i++) {
+        let wiersz = wiersze[i].dataset.distance;
+        console.log(wiersz);
+        if (wiersz > poleTxt) {
+            wiersze[i].style.display = "none";
+        }
+    }
 }
